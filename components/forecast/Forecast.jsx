1877e4300c3def9bc164e11a56a45bd1a3539d41
majdjedidi <jedidi.majd@gmail.com>
2023-07-14 17:16:56 +0100

    basic design,current weather

---
+++ components/forecast/Forecast.jsx
@@ -0,0 +1,12 @@
+import { Accordion } from 'react-accessible-accordion'
+import './forecast.css'
+export default function Forecast() {
+  return (
+    <>
+    <label className='title'>Daily</label>
+    <Accordion allowZeroExpanded>
+        
+    </Accordion>
+    </>
+  )
+}
